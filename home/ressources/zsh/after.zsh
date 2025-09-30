3261e4d5eecdcf1f59a6ef4d274cb1c308bef568
Samuel AMARAL ANTUNES <samuelamaralantunes@gmail.com>
2025-09-30 13:25:31 +0200

    fix: remove home-manager

--- home/ressources/zsh/after.zsh
+++
@@ -1,3 +0,0 @@
-nix-find-store() {
-	find /nix/store/ -maxdepth 1 -regex ".*${1}.*";
-}
--- update-home.sh
+++ update-home.sh
@@ -3,15 +3,11 @@
 cd $(dirname $0)
 
 nixpkgs_pin=$(nix eval --raw -f npins/default.nix nixpkgs)
-hm_pin=$(nix eval --raw -f npins/default.nix home-manager)
-nix_path="nixpkgs=${nixpkgs_pin}:home-manager=${hm_pin}"
-
-# Old Home-manager config (ew)
-# env NIX_PATH=${nix_path} home-manager -f ${PWD}/home/home.nix "$@"
+nix_path="nixpkgs=${nixpkgs_pin}"
 
 if [[ $# -lt 1 ]]; then
 	echo 'No arguments were provided.'
 	exit 1
 fi
 
-env NIX_PATH=${nix_path} nix run -f ./home/experimental-profile "profile.$1"
+env NIX_PATH=${nix_path} nix run -f ./home "profile.${1:-switch}"
