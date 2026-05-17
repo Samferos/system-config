70d61c08c53ccfe5df20feb5eabc11dbf5252dcb
Samuel AMARAL ANTUNES <samuelamaralantunes@gmail.com>
2026-05-17 22:38:23 +0200

    make scripts remind and ask to commit

--- update-home.sh
+++ update-home.sh
@@ -6,3 +6,24 @@ nixpkgs_pin=$(nix eval --raw -f npins/default.nix nixpkgs)
 nix_path="nixpkgs=${nixpkgs_pin}"
 
 env NIX_PATH=${nix_path} nix run -f ./home "profile.${1:-switch}"
+
+echo "Commit changes ? (git commit -a)"
+echo "[Y/n]"
+
+update_done=''
+until [ "$update_done" ]; do
+  read -r ANSWER
+  
+  case $ANSWER in
+    y | Y | yes | Yes | YES | '')
+      git commit -a
+      update_done=1
+      ;;
+    n | N | no | No | NO)
+      update_done=1
+      ;;
+    *)
+      echo 'Invalid input. Please enter y or n.'
+      ;;
+  esac
+done
