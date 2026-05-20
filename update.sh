54df8eaf1df98ea368272f461219533d43abaf2c
Samuel AMARAL ANTUNES <samuelamaralantunes@gmail.com>
2026-05-20 15:38:39 +0200

    only prompt for commit if successful

--- update.sh
+++ update.sh
@@ -8,25 +8,26 @@ shift
 nixpkgs_pin=$(nix eval --raw -f npins/default.nix nixpkgs)
 nix_path="nixpkgs=${nixpkgs_pin}:nixos-config=${PWD}/configuration.nix"
 
-doas env NIX_PATH="${nix_path}" nixos-rebuild "$command" --no-reexec "$@"
+if doas env NIX_PATH="${nix_path}" nixos-rebuild "$command" --no-reexec "$@";
+then
+  echo "Commit changes ? (git commit -a)"
+  echo "[Y/n]"
 
-echo "Commit changes ? (git commit -a)"
-echo "[Y/n]"
-
-update_done=''
-until [ "$update_done" ]; do
-  read -r ANSWER
-  
-  case $ANSWER in
-    y | Y | yes | Yes | YES | '')
-      git commit -a
-      update_done=1
-      ;;
-    n | N | no | No | NO)
-      update_done=1
-      ;;
-    *)
-      echo 'Invalid input. Please enter y or n.'
-      ;;
-  esac
-done
+  update_done=''
+  until [ "$update_done" ]; do
+    read -r ANSWER
+    
+    case $ANSWER in
+      y | Y | yes | Yes | YES | '')
+        git commit -a
+        update_done=1
+        ;;
+      n | N | no | No | NO)
+        update_done=1
+        ;;
+      *)
+        echo 'Invalid input. Please enter y or n.'
+        ;;
+    esac
+  done
+fi
